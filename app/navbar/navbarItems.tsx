0a8073045cc0282b4b8d91bee40a542514f4c100
agayushh <agayush088@gmail.com>
2025-08-04 22:21:41 +0530

    initialized project and added navbar

---
+++ app/navbar/navbarItems.tsx
@@ -0,0 +1,58 @@
+interface NavItem {
+    id: number;
+    title: string;
+    path: string;
+}
+
+export const navbarItems: NavItem[] = [
+    {
+        id: 1,
+        title: "Store",
+        path: "/",
+    },
+    {
+        id: 2,
+        title: "Mac",
+        path: "/about",
+    },
+    {
+        id: 3,
+        title: "iPad",
+        path: "/services",
+    },
+    {
+        id: 4,
+        title: "iPhone",
+        path: "/contact",
+    },
+    {
+        id: 5,
+        title: "Watch",
+        path: "/contact",
+    },
+    {
+        id: 6,
+        title: "Vision",
+        path: "/contact",
+    },
+    {
+        id: 7,
+        title: "AirPods",
+        path: "/contact",
+    },
+    {
+        id: 8,
+        title: "TV & Home",
+        path: "/contact",
+    },
+    {
+        id: 9,
+        title: "Accessories",
+        path: "/contact",
+    },
+    {
+        id: 10,
+        title: "Support",
+        path: "/contact",
+    },
+];
